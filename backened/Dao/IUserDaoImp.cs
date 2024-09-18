180b770ee091f5057549f49fe4beee1f52691991
Shreyash Patil <shreyashypatil1122002@gmail.com>
2024-09-18 16:58:24 +0530

    Add files via upload

---
+++ backened/Dao/IUserDaoImp.cs
@@ -0,0 +1,17 @@
+﻿using DotnetTask.Model;
+
+
+namespace DotnetTask.DAO
+{
+    public interface IUserDaoImp
+    {
+        public Task<List<User>> GetUsers(string host);
+        public Task<User> AddUser(User u, string host);
+
+        public Task<int> DeleteUserById(int id);
+
+        public Task<int> UpdateUserPhoneNumberById(int id, string phoneNumber);
+
+        public Task<string> SaveImage(IFormFile imageFile);
+    }
+}
